08cd4147f3d9bc7eb1aa62b24e758641ca674e86
dahial <dahial@alumni.upv.es>
2020-09-24 15:03:16 +0200

    commit desde BAT

--- threejs/practica2.js
+++ threejs/practica2.js
@@ -49,7 +49,7 @@ function loadScene() {
 	var material_suelo = new THREE.MeshBasicMaterial({color:'gray', wireframe:true});
 
 	// Geometrías
-	var geo_plano = new THREE.PlaneGeometry(0,200,200);
+	var geo_plano = new THREE.PlaneGeometry(1000,1000,10);
 
 	var geo_base = new THREE.CylinderGeometry(50, 50, 15, 50);
 
