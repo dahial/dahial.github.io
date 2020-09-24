0291f87a8f28c5d794f6103f4293ab602d17fffc
dahial <dahial@alumni.upv.es>
2020-09-24 20:36:36 +0200

    commit desde BAT

--- threejs/practica2.js
+++ threejs/practica2.js
@@ -140,16 +140,14 @@ function loadScene() {
 	mano.position.y = 83;
 
 	// NERVIOS
-	nervio1.position.x = -10;
-	nervio1.position.z = -10;
-	//nervio1.translate(-10, 0, -10);
-	//nervio2.translate(-100, 0, 100);
-	//nervio3.translate(100, 0, 100);
-	//nervio4.translate(100, 0, -100);
+	nervio1.position.set(-10,0,-10);
+	nervio2.position.set(-100, 0, 100);
+	nervio3.position.set(100, 0, 100);
+	nervio4.position.set(100, 0, -100);
 	nervio.attach(nervio1);
-	//nervio.attach(nervio2);
-	//nervio.attach(nervio3);
-	//nervio.attach(nervio4);
+	nervio.attach(nervio2);
+	nervio.attach(nervio3);
+	nervio.attach(nervio4);
 	nervio.position.y = 43;
 
 	// ANTEBRAZO
