790e71acd22f79fe2b92d04e7c7956ad1e6fd8cc
dahial <dahial@alumni.upv.es>
2020-10-14 16:26:19 +0200

    commit desde BAT

--- threejs/practica4.js
+++ threejs/practica4.js
@@ -11,6 +11,13 @@ var effectController;
 // Objetos y tiempo
 var antes = Date.now();
 
+// Variables globales
+var suelo, robot;
+var l = b = -150;
+var r = t = -l;
+var planta;
+var init_poi = new THREE.Vector3(0,100,0);
+
 // Acciones a realizar
 init();
 loadScene();
@@ -52,39 +59,149 @@ function init()
 
 function loadScene()
 {
+	// Construir el grafo de escena
+
 	// Materiales
-	var material = new THREE.MeshBasicMaterial( 
-                                      { color:0xFFFFFF,
-                                        wireframe: true } );
+	var material = new THREE.MeshBasicMaterial({color:'white', wireframe:true});
+	var material_suelo = new THREE.MeshBasicMaterial({color:'gray', wireframe:true});
 
-	// Peonza
-	peonza = new THREE.Object3D();
-	var cuerpo = new THREE.Mesh(new THREE.CylinderGeometry( 1, 0.2, 2, 10, 2 ), material);
-	cuerpo.position.y = 1.5;
-	peonza.add( cuerpo );
-	var punta = new THREE.Mesh(new THREE.CylinderGeometry( 0.1, 0, 0.5, 10, 1 ), material);
-	punta.position.set( 0, 0.25, 0 );
-	peonza.add( punta );
-	var mango = new THREE.Mesh(new THREE.CylinderGeometry( 0.1, 0.1, 0.5, 10, 1 ), material);
-	mango.position.set( 0, 2.75, 0 );
-	peonza.add( mango );
-	peonza.rotation.x = Math.PI/16;
+	// Geometrías
+	var geo_suelo = new THREE.PlaneGeometry(1000,1000,20,20);
 
-	eje = new THREE.Object3D();
-	eje.position.set(-2.5,0,-2.5);
-	eje.add( peonza );
-	scene.add(eje);
+	var geo_base = new THREE.CylinderGeometry(50, 50, 15, 40);
+
+	var geo_eje = new THREE.CylinderGeometry(20, 20, 18, 30);
+	var geo_esparrago = new THREE.BoxGeometry(18, 120, 12);
+	var geo_rotula = new THREE.SphereGeometry(20, 15, 15);
+
+	var geo_disco = new THREE.CylinderGeometry(22, 22, 6, 30);
+	var geo_nervio = new THREE.BoxGeometry(4,80,4);
+	var geo_mano = new THREE.CylinderGeometry(15, 15, 40, 30);
+
+	// Geometría personalizada para las pinzas
+	var geo_pinza = new THREE.Geometry();
+
+	geo_pinza.vertices.push(
+		new THREE.Vector3(-2, 10, 0),	//0
+		new THREE.Vector3(2, 10, 0),	//1
+		new THREE.Vector3(2, -10, 0),	//2
+		new THREE.Vector3(-2, -10, 0),	//3
+		new THREE.Vector3(-2, 10, 19),	//4
+		new THREE.Vector3(2, 10, 19),	//5
+		new THREE.Vector3(2, -10, 19),	//6
+		new THREE.Vector3(-2, -10, 19),	//7
+		new THREE.Vector3(-2, 5, 38),	//8
+		new THREE.Vector3(0, 5, 38),	//9
+		new THREE.Vector3(0, -5, 38),	//10
+		new THREE.Vector3(-2, -5, 38)	//11
+	);
+
+	geo_pinza.faces.push(
+		new THREE.Face3(0,1,2),
+		new THREE.Face3(0,2,3),
+		new THREE.Face3(0,4,7),
+		new THREE.Face3(0,3,7),
+		new THREE.Face3(5,2,1),
+		new THREE.Face3(5,2,6),
+		new THREE.Face3(0,1,4),
+		new THREE.Face3(5,1,4),
+		new THREE.Face3(3,2,7),
+		new THREE.Face3(2,7,6),
+
+		new THREE.Face3(4,5,8),
+		new THREE.Face3(5,8,9),
+		new THREE.Face3(4,8,7),
+		new THREE.Face3(8,7,11),
+		new THREE.Face3(5,9,6),
+		new THREE.Face3(9,6,10),
+		new THREE.Face3(8,9,11),
+		new THREE.Face3(10,9,11),
+		new THREE.Face3(7,6,11),
+		new THREE.Face3(6,11,10)
+	);
 
 
-	// Suelo
-	var geoSuelo = new THREE.PlaneGeometry( 5, 5 );
-	var suelo = new THREE.Mesh( geoSuelo, material );
-	suelo.rotation.x = -Math.PI/2;
-	scene.add( suelo );
+	// Objetos
+
+	suelo = new THREE.Mesh(geo_suelo, material_suelo);
+	robot = new THREE.Object3D();
+
+	var pinzaIz = new THREE.Mesh(geo_pinza, material);
+	var pinzaDe = pinzaIz.clone();
+
+	var mano = new THREE.Mesh(geo_mano, material );
+	var nervio1 = new THREE.Mesh(geo_nervio, material);
+	var nervio2 = nervio1.clone();
+	var nervio3 = nervio2.clone();
+	var nervio4 = nervio3.clone();
+	var nervio = new THREE.Object3D();
+	var disco = new THREE.Mesh(geo_disco, material);
+
+	var rotula = new THREE.Mesh(geo_rotula, material);
+	var esparrago = new THREE.Mesh(geo_esparrago, material);
+	var eje = new THREE.Mesh(geo_eje, material);
+
+	var antebrazo = new THREE.Object3D();
+	var brazo = new THREE.Object3D();
+
+	var base = new THREE.Mesh(geo_base, material);
+
+	//// Transformaciones y creación del grafo de escena
+	//// (Diseño bottom-up aprovechando transformaciones de padres)
+	suelo.rotation.x = Math.PI/2;
+
+	// MANO
+	pinzaIz.position.x = -15;
+	pinzaDe.position.x = 15;
+	pinzaDe.rotation.z = Math.PI;
+
+	mano.rotation.z = Math.PI/2;
+
+	mano.attach(pinzaIz);
+	mano.attach(pinzaDe);
+	mano.position.y = 83;
+
+	// NERVIOS
+	nervio1.position.set(-10,0,-10);
+	nervio2.position.set(-10, 0, 10);
+	nervio3.position.set(10, 0, 10);
+	nervio4.position.set(10, 0, -10);
+	nervio.attach(nervio1);
+	nervio.attach(nervio2);
+	nervio.attach(nervio3);
+	nervio.attach(nervio4);
+	nervio.position.y = 43;
+
+	// ANTEBRAZO
+	antebrazo.attach(mano);
+	antebrazo.attach(nervio);
+	antebrazo.attach(disco);
+
+	// BRAZO
+	antebrazo.position.y = 120;
+	rotula.position.y = 120;
+	esparrago.position.y = 60;
+
+	brazo.attach(antebrazo);
+	brazo.attach(rotula);
+	brazo.attach(esparrago);
+	brazo.attach(eje);
+
+	// BASE
+	brazo.position.y = 3;
+	base.attach(brazo);
+
+	// ROBOT
+	base.position.y = 7.5;
+	robot.attach(base);
+
+	// Organizacion de la escena
+	scene.add(suelo);
+	scene.add(robot);
 	
 	//Coordinates.drawGrid({size:6,scale:1});
-	Coordinates.drawGrid({size:6,scale:1, orientation:"y"});
-	Coordinates.drawGrid({size:6,scale:1, orientation:"z"});	
+	//Coordinates.drawGrid({size:6,scale:1, orientation:"y"});
+	//Coordinates.drawGrid({size:6,scale:1, orientation:"z"});	
 
 }
 
