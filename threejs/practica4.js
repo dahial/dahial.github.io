adefce2ceea9919c8679a0201e1fe3e916dbfda7
dahial <dahial@alumni.upv.es>
2020-10-14 18:25:11 +0200

    commit desde BAT

--- threejs/practica4.js
+++ threejs/practica4.js
@@ -268,8 +268,8 @@ function updateRobot(){
 	scene.getObjectByName("antebrazo", true).rotation.y = effectController.giroAntebrazoY * Math.PI / 180;
 	scene.getObjectByName("antebrazo", true).rotation.x = effectController.giroAntebrazoZ * Math.PI / 180;
 	scene.getObjectByName("mano", true).rotation.x = - effectController.giroPinza * Math.PI / 180;
-	scene.getObjectByName("pinzaIz", true).position.x = - effectController.separacionPinza;
-	scene.getObjectByName("pinzaDe", true).position.x = effectController.separacionPinza;
+	scene.getObjectByName("pinzaIz", true).position.y = - effectController.separacionPinza;
+	scene.getObjectByName("pinzaDe", true).position.y = effectController.separacionPinza;
 
 
 }
