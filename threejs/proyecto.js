dd1fbebbb46a64a7118adf2511a396c3246058bf
dahial <dahial@alumni.upv.es>
2020-10-20 19:35:57 +0200

    commit desde BAT

--- threejs/proyecto.js
+++ threejs/proyecto.js
@@ -83,7 +83,7 @@ function loadScene()
     // Construir la escena
 
     // Construir el suelo
-    var groundTexture = loader.load("https://dahial.github.io/images/proyecto/ground_diffuse.png");
+    var groundTexture = loader.load(['https://dahial.github.io/images/proyecto/ground_diffuse.png',]);
     groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
     groundTexture.repeat.set(25,25);
     groundTexture.anisotropy = 16;
