74a8d627d70a549ddebfa49377fcadacc3d70b1e
dahial <dahial@alumni.upv.es>
2020-10-21 05:07:58 +0200

    commit desde BAT

--- threejs/proyecto.js
+++ threejs/proyecto.js
@@ -308,7 +308,7 @@ function update()
 
 	// Camara sigue al usuario
 	camera.position = camera.position.lerp(player.position, 0.01) -  playerDirection * cameraDistance; // Coloca la cámara detrás del usuario
-	camera.lookAt(player);
+	//camera.lookAt(player);
 
 	// Actualiza los FPS
 	//stats.update();
