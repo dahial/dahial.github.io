a66660a33dd901fbbdaa967dabf96715ed3ef3c5
dahial <dahial@alumni.upv.es>
2020-10-21 03:08:16 +0200

    commit desde BAT

--- threejs/proyecto.js
+++ threejs/proyecto.js
@@ -90,7 +90,31 @@ function loadScene()
 	const gltfloader = new THREE.GLTFLoader();
 
 	// Cargar jugador
-	var player = loader.load('../models/proyecto/spaceship.gltf')
+	var player = loader.load('../models/proyecto/spaceship.gltf',
+	// called when the resource is loaded
+	function ( gltf ) {
+
+		scene.add( gltf.scene );
+
+		gltf.animations; // Array<THREE.AnimationClip>
+		gltf.scene; // THREE.Group
+		gltf.scenes; // Array<THREE.Group>
+		gltf.cameras; // Array<THREE.Camera>
+		gltf.asset; // Object
+
+	},
+	// called while loading is progressing
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	});
 	player.position = new THREE.Vector3(500,500,500);
 	scene.add(playerModel);
 
