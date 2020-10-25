164a3db4c853d2267accf311fbad50d2345d68df
dahial <dahial@alumni.upv.es>
2020-10-25 02:39:33 +0200

    commit desde BAT

--- threejs/practica5.js
+++ threejs/practica5.js
@@ -195,7 +195,7 @@ function loadScene()
 		new THREE.Face3(0,4,7),
 		new THREE.Face3(0,3,7),
 		new THREE.Face3(5,2,1),
-		new THREE.Face3(5,6,2),
+		new THREE.Face3(5,2,6),
 		new THREE.Face3(0,4,1),
 		new THREE.Face3(5,1,4),
 		new THREE.Face3(3,2,7),
